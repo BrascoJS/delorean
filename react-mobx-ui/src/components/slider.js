7e04ec6521aa590ea7072ccf96d6acf91be12ece
Morgan Pierson <morganpierson@gmail.com>
2017-01-25 16:38:03 -0800

    state stepper changed to linear

--- react-mobx-ui/src/components/slider.js
+++ react-mobx-ui/src/components/slider.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import Slider from 'material-ui/Slider';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
-injectTapEventPlugin();
-
-const testArray = [1,2,3,4,5,6]
-const stepVal = 1 / testArray.length
+/**
+ * By default, the slider is continuous.
+ * The `step` property causes the slider to move in discrete increments.
+ */
+const array = [1,2,3,4,5,,3,4,3,2,4,5,4,3,4,4,2,4,3,4,3,4,3,4,3,4,5,5,4,4]
+const stepNum = 1 / array.length
 const SliderExampleStep = () => (
-  <div>
-    <Slider />
-  </div>
-
+  <Slider step={stepNum} value={0} />
 );
 
 export default SliderExampleStep;
