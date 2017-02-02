fdf62c830471d77df6d61d3b1c9e8d6cdbf1e372
listentomm <maxmarcususc@gmail.com>
2017-02-01 21:10:57 -0800

    removed unfinished features

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import mobx from 'mobx';
-import mobxReact from 'mobx-react';
 import Toolbar from './components/toolbar';
 import SliderExampleStep from './components/slider';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -21,8 +19,6 @@ export default class Delorean extends Component {
 
   componentDidMount() {
     this.getData();
-    mobxReact.trackComponents();
-    document.body.addEventListener('click', this.treeClick, true);
   }
 
   getData() {
@@ -36,39 +32,6 @@ export default class Delorean extends Component {
     message.dispatch = action;
     handleMessages(message, { [message.instanceId]: true }, 1);
   }
-
-  findComponentAndNode = target => {
-    let node = target;
-    console.log('node: ', node);
-    let component;
-    while (node) {
-      console.log('registry: ', mobxReact.componentByNodeRegistery);
-      component = mobxReact.componentByNodeRegistery.get(node);
-      if (component) return { component, node };
-      node = node.parentNode;
-    }
-    return { component: undefined, node: undefined };
-  }
-
-  treeClick = e => {
-    // if (state.graphEnabled) {
-    if (e) {
-      const target = e.target;
-      const component = this.findComponentAndNode(target).component;
-      if (component) {
-        e.stopPropagation();
-        e.preventDefault();
-        const dependencyTree = mobx.extras.getDependencyTree(component.render.$mobx);
-        // deduplicateDependencies(dependencyTree);
-        // setGlobalState({
-        //   dependencyTree,
-        //   hoverBoxes: [],
-        //   graphEnabled: false,
-        // });
-        console.log(dependencyTree);
-      }
-    }
-  };
 
   render() {
     return (
